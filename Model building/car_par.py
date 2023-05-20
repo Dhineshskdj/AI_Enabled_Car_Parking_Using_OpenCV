9946924992f17278b85f4412eadc7102a31480c7
dhinesh <dhineshcollege@gmail.com>
2023-05-20 09:52:11 +0530

    commit 5

--- Model building/car_par.py
+++ Model building/car_par.py
@@ -32,8 +32,7 @@ def checkParkingSpace(imgPro):
             thickness = 2
 
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
-        cvzone.putTextRect(img, str(count), (x, y + height - 3), scale=1,
-                           thickness=2, offset=0, colorR=color)
+
 
     cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (100, 50), scale=3,
                            thickness=5, offset=20, colorR=(200,0,0))
